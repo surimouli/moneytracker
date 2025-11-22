5dcacaab6d8f939cb5b8b3965d5f7216e1611c79
Mouli Suri <surimi@mail.uc.edu>
2025-11-21 22:43:06 -0500

    cleanup

--- proxy.js
+++
@@ -1,9 +0,0 @@
-// proxy.js
-import { clerkMiddleware } from '@clerk/nextjs/server';
-
-export default clerkMiddleware();
-
-export const config = {
-  // Apply Clerk to everything except static files and Next internals
-  matcher: ['/((?!_next|.*\\..*).*)'],
-};
